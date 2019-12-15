fb261226cc0ec3411fb050e86f7225f4b38f6d0f
Rudolph Bott <rbott@defiant.ganeti.org>
2019-12-15 20:43:43 +0100

    move log folder preperation to separate function

--- run-cluster-test.sh
+++ run-cluster-test.sh
@@ -26,6 +26,13 @@ usage() {
 	exit 1
 }
 
+prepareLogDirectory() {
+	LOGPATH=${LOGBASE}/${CLUSTERTYPE}/$(date --utc +%F_%H-%M-%S)/
+	mkdir -p "${LOGPATH}"
+	rm -f "${LOGBASE}/${CLUSTERTYPE}/latest"
+	ln -s "${LOGPATH}" "${LOGBASE}/${CLUSTERTYPE}/latest"
+}
+
 echoAndLog() {
 	logLine=$@
 	echo "${logLine}" | tee -a "${LOGPATH}/main.log"
@@ -136,7 +143,9 @@ runQaScript() {
     scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "${tmpkey}" qa-configs/${recipe}.json 192.168.122.11:/tmp/
     ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "${tmpkey}" -t 192.168.122.11 "export PYTHONPATH=\"/usr/share/ganeti/testsuite/:/usr/share/ganeti/default\"; cd /usr/share/ganeti/testsuite/qa; ./ganeti-qa.py --yes-do-it /tmp/${recipe}.json"
     qaScriptReturnCode=$?
-    scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "${tmpkey}" 192.168.122.11:/var/log/ganeti/qa-output.log "${LOGPATH}/qa-script.log"
+    scp -r -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i "${tmpkey}" 192.168.122.11:/var/log/ganeti "${LOGPATH}" > /dev/null
+    chmod a+x "${LOGPATH}ganeti"
+    chmod -R a+r "${LOGPATH}"
     rm "${tmpkey}"
     return $qaScriptReturnCode
 }
@@ -176,8 +185,7 @@ case $CLUSTERTYPE in
 		;;
 esac
 
-LOGPATH=${LOGBASE}/${CLUSTERTYPE}/$(date --utc +%F_%H-%M-%S)/
-mkdir -p "${LOGPATH}"
+prepareLogDirectory
 
 if checkLock; then
 	if ! acquireLock; then exit 1; fi
